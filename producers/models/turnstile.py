1289fdbaebd558fff1fa43961c09286ec5caf89a
pquinones <pablo.1445@gmail.com>
2020-04-12 13:46:33 -0500

    Turnstile Produce Station Name modified

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -58,7 +58,7 @@ class Turnstile(Producer):
             key={"timestamp": self.time_millis()},
             value={
                  "station_id": self.station.station_id,
-                 "station_name": Turnstile.station_name,
+                 "station_name": self.station.name,
                  "line": self.station.color.name
             }
         )
