4f855e2fdcd031e02dda524136b381d599a9c6c0
pquinones <pablo.1445@gmail.com>
2020-04-12 20:30:49 -0500

    copy repo sh

---
+++ run/00_copy_repo.sh
@@ -0,0 +1,6 @@
+rm -rf consumers/
+rm -rf producers/
+
+cp -R /home/workspace/Optimizing-Public-Trans.-UDACITY/consumers .
+cp -R /home/workspace/Optimizing-Public-Trans.-UDACITY/producers .
+cp -R /home/workspace/Optimizing-Public-Trans.-UDACITY/run .
