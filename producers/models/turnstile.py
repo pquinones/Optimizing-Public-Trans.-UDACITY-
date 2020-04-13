354d08e4042b9537dea71a865fa4037d5d880406
pquinones <pablo.1445@gmail.com>
2020-04-12 19:27:05 -0500

    Topics Reformatted

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -35,7 +35,7 @@ class Turnstile(Producer):
         #
         #
         super().__init__(
-            f"chicago.system.turnstiles.{station_name}", # TODO: Come up with a better topic name
+            f"org.chicago.cta.station.turnstiles.{station_name}", # TODO: Come up with a better topic name
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
             num_partitions=3,
